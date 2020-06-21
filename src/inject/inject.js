1720640868f4fa22a7227308c11d23f1f384da6b
Thomas Zhang <thomaszhang@berkeley.edu>
2020-06-20 21:34:05 -0700

    Major refactoring to readable functions

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,17 +1,20 @@
+const googleCalendarTabEventDetailsId = 'tabEventDetails';
+const addingCalendarEventMessage = 'AddingCalendarEvent';
+
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 	  // listen for messages sent from background.js
-	  if (request.message === 'AddingCalendarEvent') {
+	  if (request.message === addingCalendarEventMessage) {
 		  if (request.url.toString().indexOf('https://calendar.google.com/calendar/r/eventedit/' > 0)) {
-			waitForElement("tabEventDetails", function(){
-				addButton();
+			waitForElement(googleCalendarTabEventDetailsId, function(){
+				addButtonDivWithIcon();
 			});
 		  }
 	  }
   });
 
 function waitForElement(id, callback){
-    var poops = setInterval(function(){
+    const poops = setInterval(function(){
         if(document.getElementById(id)){
             clearInterval(poops);
             callback();
@@ -19,94 +22,103 @@ function waitForElement(id, callback){
     }, 100);
 }
 
-waitForElement("tabEventDetails", function(){
-    addButton();
+waitForElement(googleCalendarTabEventDetailsId, function(){
+    addButtonDivWithIcon();
 });
-function addButton() {
-	var item = document.getElementById("tabEventDetails")
-		var button = document.createElement("button");
-		var img = document.createElement('img'); 
-		img.src = chrome.extension.getURL('src/inject/icon.png');
-		img.style.margin = '0px 0px 0px 26px';
-		button.style.width = '164px';
-		button.style.margin = '4px 0px 4px 22px';
-		button.style.height = '36px';
-		button.innerHTML = "Add Meeting Costs";
-		button.id = 'meetingCost';
-		button.addEventListener('click',function(){
-			// console.log('btnComment worked')
-			var titleInput = document.getElementById('xTiIn');
-			var descriptionInput = document.querySelector('[aria-label="Description"]');
-			
-			
-			var startTimeElement = document.querySelector('[aria-label="Start time"]');
-			var startDateElement = document.querySelector('[aria-label="Start date"]');
-			const startTimeText = `${startDateElement.value} ${startTimeElement.value.slice(0, -2)} ${startTimeElement.value.slice(-2, startTimeElement.value.length)}`;
-			// console.log(startTimeText);
-			const startDate = Date.parse(startTimeText);
-			// console.log(startDate);
-			
-			
-			var endTimeElement = document.querySelector('[aria-label="End time"]');
-			var endDateElement = document.querySelector('[aria-label="End date"]');
-			const endTimeText = `${endDateElement.value} ${endTimeElement.value.slice(0, -2)} ${endTimeElement.value.slice(-2, endTimeElement.value.length)}`;
-			// console.log(endTimeText);
-			const endDate = Date.parse(endTimeText);
-			// console.log(endDate);
-			
-			
-			var participants = document.getElementById('xDetDlgAtt');
-			var participantsElements = document.querySelector('[aria-label="Guests invited to this event."]');
-			// console.log(participants);
-			// console.log(participantsElements.childElementCount);
-			let numberOfParticipants;
-			if (participants) {
-				numberOfParticipants = participants.innerHTML.match(/([\d.]+) *guests/)[1];
-			}
-			else if (participantsElements) {
-				numberOfParticipants = participantsElements.childElementCount;
-			}
-			if (numberOfParticipants <= 0) {
-				numberOfParticipants = 1;
-			}
-			var durationInHours = (endDate - startDate) / (1000 * 3600);
-			var costPerHour = 100;
-			// console.log(`The duration in hours is: ${durationInHours}`);
-			// console.log(`There are ${numberOfParticipants} participants at the meeting`);
 
-			// var defaultTexts = document.querySelectorAll('[jsname="LwH6nd"]');
-			// for (var defaultText of defaultTexts) {
-			// 	if (defaultText && defaultText.innerHTML === 'Add description') {
-			// 		defaultText.innerHTML = '';
-			// 	}
-			// }
-			var defaultTexts = document.querySelectorAll('[jsname="V67aGc"]');
-			for (var defaultText of defaultTexts) {
-				if (defaultText && defaultText.innerHTML === 'Add description') {
-					defaultText.innerHTML = '';
-				}
-			}
-			// titleInput.value = `[Meeting cost: $${numberOfParticipants * costPerHour * durationInHours}] - ${titleInput.value}`;
-			// titleInput.focus();
-			descriptionInput.innerHTML = `[Meeting cost: $${numberOfParticipants * costPerHour * durationInHours}] - ${descriptionInput.innerHTML}`;
-		});
-		div = document.createElement('div');
-		div.id = 'meetingCostDiv';
-		div.appendChild(img)
-		div.appendChild(button)
-		if (!document.getElementById('meetingCostDiv')) {
-			item.prepend(div);
-		}
+function addButtonDivWithIcon() {
+	const button = createCostButton();
+	const icon = createCostIcon();
+	const div = createCostDiv(icon, button);
+	if (!document.getElementById('meetingCostDiv')) {
+		const googleCalendarTabEventElement = document.getElementById(googleCalendarTabEventDetailsId);
+		googleCalendarTabEventElement.prepend(div);
+	}
 }
 
-// chrome.extension.sendMessage({}, function(response) {
-// 	var readyStateCheckInterval = setInterval(function() {
-// 	if (document.readyState === "complete") {
-// 		clearInterval(readyStateCheckInterval);
-// 		// ----------------------------------------------------------
-// 		// This part of the script triggers when page is done loading
-// 		// ----------------------------------------------------------
+function createCostDiv(icon, button) {
+	const div = document.createElement('div');
+	div.id = 'meetingCostDiv';
+	div.appendChild(icon);
+	div.appendChild(button);
+	return div;
+}
 
-// 	}
-// 	}, 10);
-// });
+function createCostIcon() {
+	const icon = document.createElement('img');
+	icon.src = chrome.extension.getURL('src/inject/icon.png');
+	icon.style.margin = '0px 0px 0px 26px';
+	return icon;
+}
+
+function createCostButton() {
+	const button = document.createElement('button');
+	button.style.width = '164px';
+	button.style.margin = '4px 0px 4px 22px';
+	button.style.height = '36px';
+	button.innerHTML = 'Add Meeting Costs';
+	button.id = 'meetingCost';
+	button.addEventListener('click', buttonClickEvent);
+	return button;
+}
+
+function buttonClickEvent() {
+	const descriptionInputElement = document.querySelector('[aria-label="Description"]');
+	const startDate = parseDomStartDate();
+	const endDate = parseDomEndDate();
+	const numberOfParticipants = parseNumberOfParticipants();
+	const durationInHours = calculateDurationInHours(endDate, startDate);
+	const costPerHour = 100;
+	removePlaceholderDescriptionText();
+	prependDescriptionTextWithMeetingCosts(descriptionInputElement, numberOfParticipants, costPerHour, durationInHours);
+}
+
+function prependDescriptionTextWithMeetingCosts(descriptionInputElement, numberOfParticipants, costPerHour, durationInHours) {
+	descriptionInputElement.innerHTML = `[Meeting cost: $${numberOfParticipants * costPerHour * durationInHours}] - ${descriptionInputElement.innerHTML}`;
+}
+
+function removePlaceholderDescriptionText() {
+	const defaultTexts = document.querySelectorAll('[jsname="V67aGc"]');
+	for (const defaultText of defaultTexts) {
+		if (defaultText && defaultText.innerHTML === 'Add description') {
+			defaultText.innerHTML = '';
+		}
+	};
+}
+
+function calculateDurationInHours(endDate, startDate) {
+	return (endDate - startDate) / (1000 * 3600);
+}
+
+function parseNumberOfParticipants() {
+	const participants = document.getElementById('xDetDlgAtt');
+	const participantsElements = document.querySelector('[aria-label="Guests invited to this event."]');;
+	let numberOfParticipants;
+	if (participants) {
+		numberOfParticipants = participants.innerHTML.match(/([\d.]+) *guests/)[1];
+	}
+	else if (participantsElements) {
+		numberOfParticipants = participantsElements.childElementCount;
+	}
+	if (numberOfParticipants <= 0) {
+		numberOfParticipants = 1;
+	}
+	return numberOfParticipants;
+}
+
+function parseDomEndDate() {
+	const endTimeElement = document.querySelector('[aria-label="End time"]');
+	const endDateElement = document.querySelector('[aria-label="End date"]');
+	const endTimeText = `${endDateElement.value} ${endTimeElement.value.slice(0, -2)} ${endTimeElement.value.slice(-2, endTimeElement.value.length)}`;
+	const endDate = Date.parse(endTimeText);
+	return endDate;
+}
+
+function parseDomStartDate() {
+	const startTimeElement = document.querySelector('[aria-label="Start time"]');
+	const startDateElement = document.querySelector('[aria-label="Start date"]');
+	const startTimeText = `${startDateElement.value} ${startTimeElement.value.slice(0, -2)} ${startTimeElement.value.slice(-2, startTimeElement.value.length)}`;
+	const startDate = Date.parse(startTimeText);
+	return startDate;
+}
+
